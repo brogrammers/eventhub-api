658d08381a77094883d2b92b81108979473cf757
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-06-12 22:59:42 +0100

    initial commit. Includes rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventhubApi::Application.config.secret_token = '0278d1122133d10bda1ab2d648cbfb724d8d152cbe2757ed3387dac945ce7e6afb279b24df14b9fc9d587938fa8f1de4de3ec9ac945aa1568ac6417bf256566a'
