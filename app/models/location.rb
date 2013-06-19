addc748d3cf1d8c671b21b0f25df435a723701df
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-06-19 23:00:33 +0100

    added validation to location model + validation error messages + test

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,5 +3,6 @@ class Location < ActiveRecord::Base
 
   belongs_to :locationable, :polymorphic => true
 
-  validates :latitude, :longitude, :presence => true
+  validates :latitude , numericality: { greater_than:  -90, less_than:  90 }
+  validates :longitude, numericality: { greater_than: -180, less_than: 180 }
 end
