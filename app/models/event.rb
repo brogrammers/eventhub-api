8cf2ffb76726e369f17cc7838bde0bf9c75e8199
robertlis <robert.lis2@mail.dcu.ie>
2013-06-22 22:42:46 +0100

    added tests to be implemented and some validations on event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,27 @@
-class Event < ActiveRecord::Base
-  attr_accessible :description, :end_time, :name, :start_time
+class EventTimesValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.start_time > Time.now
+      record.errors[:start_time] << 'event start time must be in the future'
+    end
 
-  #detinations stuff, save destination object & reload place before you can see this being updated,
-  #make sure both destination AND event have their id's assigned
+    unless record.end_time > record.start_time
+      record.errors[:end_time] << 'event must end after it starts'
+    end
+  end
+end
+
+class Event < ActiveRecord::Base
+  attr_accessible :description, :end_time, :start_time, :name, :description
+
   has_many :destinations, :as => :choice, :dependent => :destroy
 
   has_many :comments, :as => :commentable, :dependent => :destroy
   has_many :offers ,:as => :offerer, :dependent => :destroy
 
   belongs_to :place
+
+  validates :description, :name, :start_time, :end_time, :place, :presence => true
+  validates :name, :length => { :minimum => 5, :maximum => 256 }
+  validates :description, :length => { :minimum => 5, :maximum => 1024 }
+  validates_with EventTimesValidator
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,63 +1,120 @@
 require 'spec_helper'
 
 describe Event do
-  fixtures :events, :comments, :offers
+  fixtures :events, :comments, :offers, :places
 
-  it 'should be possible to comment on an event' do
-    event = events :one
-    comment = comments :one
-    event.comments << comment
-    comment.save!
-    event.save!
-    event.comments.size.should == 1
+  context 'Comments' do
+
+    it 'should be possible to comment on an event' do
+      event = events :one
+      comment = comments :one
+      event.comments << comment
+      comment.save!
+      event.save!
+      event.comments.size.should == 1
+    end
+
+    it 'should destroy all comments of an event once the event is destroyed' do
+      event = events :one
+      comment = comments :one
+      event.comments << comment
+      comment.save!
+      event.save!
+      event.destroy
+      Comment.all.size.should == 0
+    end
+
   end
 
-  it 'should destroy all comments of an event once the event is destroyed' do
-    event = events :one
-    comment = comments :one
-    event.comments << comment
-    comment.save!
-    event.save!
-    event.destroy
-    Comment.all.size.should == 0
+  context 'Offers' do
+
+    it 'should be possible to add an offer to an event' do
+      event = events :one
+      offer1, offer2 = offers(:one), offers(:two)
+      event.offers << offer1
+      event.offers << offer2
+      event.save!
+      offer1.save!
+      offer2.save!
+      event.offers.should include(offer1)
+      event.offers.should include(offer2)
+      offer1.offerer.should == event
+      offer2.offerer.should == event
+    end
+
+    it 'should be possible to remove an offer from an event' do
+      event = events :one
+      offer1, offer2 = offers(:one), offers(:two)
+      event.offers << offer1
+      event.offers << offer2
+      event.save!
+      offer1.save!
+      offer2.save!
+      event.offers.delete offer1
+      event.offers.delete offer2
+      event.offers.size.should == 0
+    end
+
+    it 'should destroy all offers of an event once the event is destroyed' do
+      event = events :one
+      offer1, offer2 = offers(:one), offers(:two)
+      event.offers << offer1
+      event.offers << offer2
+      event.save!
+      offer1.save!
+      offer2.save!
+      event.destroy
+      expect { Offer.find offer1.id; Offer.find offer2.id }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
   end
 
-  it 'should be possible to add an offer to an event' do
-    event = events :one
-    offer1, offer2 = offers(:one), offers(:two)
-    event.offers << offer1
-    event.offers << offer2
-    event.save!
-    offer1.save!
-    offer2.save!
-    event.offers.should include(offer1)
-    event.offers.should include(offer2)
-    offer1.offerer.should == event
-    offer2.offerer.should == event
+  context 'Validations' do
+    it 'should not be possible to create event without description' do
+
+    end
+
+    it 'should not be possible to create event without name' do
+
+    end
+
+    it 'should not be possible to create event without start time' do
+
+    end
+
+    it 'should not be possible to create event without end time' do
+
+    end
+
+    it 'should not be possible to create event without place' do
+
+    end
+
+    it 'should not be possible to create event name containing less than 5 characters' do
+
+    end
+
+    it 'should not be possible to create event name containing more than 256 characters' do
+
+    end
+
+    it 'should not be possible to create event descripion containing less than 5 characters' do
+
+    end
+
+    it 'should not be possible to create event description containing more than 1024 charcters' do
+
+    end
+
+    it 'should not be possible to create event which starts in the past' do
+
+    end
+
+    it 'should not be possible to crate event which ends before it starts' do
+
+    end
+
+
   end
 
-  it 'should be possible to remove an offer from an event' do
-    event = events :one
-    offer1, offer2 = offers(:one), offers(:two)
-    event.offers << offer1
-    event.offers << offer2
-    event.save!
-    offer1.save!
-    offer2.save!
-    event.offers.delete offer1
-    event.offers.delete offer2
-    event.offers.size.should == 0
-  end
-
-  it 'should destroy all offers of an event once the event is destroyed' do
-    event = events :one
-    offer1, offer2 = offers(:one), offers(:two)
-    event.offers << offer1
-    event.offers << offer2
-    event.save!
-    offer1.save!
-    offer2.save!
-    event.destroy
-    expect { Offer.find offer1.id; Offer.find offer2.id }.to raise_error(ActiveRecord::RecordNotFound)
-  end
 end
