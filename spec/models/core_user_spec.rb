561fdec88472eb6e4bced56234c947090161aeb6
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-06-28 14:06:24 +0100

    separated tests in core user spec

--- spec/models/core_user_spec.rb
+++ spec/models/core_user_spec.rb
@@ -8,84 +8,92 @@ describe CoreUser do
     expect { user.save! }.to raise_error(ActiveRecord::RecordInvalid)
   end
 
-  it 'should add a device to the user record' do
-    user = users :one
-    device = devices :one
-    user.devices << device
-    user.save!.should be_true
-    device.save!.should be_true
-    device.user(true).should be_a(User)
-    user.devices(true).should include(device)
+  context 'devices' do
+
+    it 'should add a device to the user record' do
+      user = users :one
+      device = devices :one
+      user.devices << device
+      user.save!.should be_true
+      device.save!.should be_true
+      device.user(true).should be_a(User)
+      user.devices(true).should include(device)
+    end
+
+    it 'should add a device to the business user record' do
+      user = business_users :one
+      device = devices :one
+      user.devices << device
+      user.save!.should be_true
+      device.save!.should be_true
+      device.user(true).should be_a(BusinessUser)
+      user.devices(true).should include(device)
+    end
+
+    it 'should destroy all devices once the user is destroyed' do
+      user = users :one
+      device = devices :one
+      user.devices << device
+      user.save!.should be_true
+      device.save!.should be_true
+      user.destroy
+      expect { Device.find device.id }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'should destroy all devices once the business user is destroyed' do
+      user = business_users :one
+      device = devices :one
+      user.devices << device
+      user.save!.should be_true
+      device.save!.should be_true
+      user.destroy
+      expect { Device.find device.id }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
   end
 
-  it 'should add a device to the business user record' do
-    user = business_users :one
-    device = devices :one
-    user.devices << device
-    user.save!.should be_true
-    device.save!.should be_true
-    device.user(true).should be_a(BusinessUser)
-    user.devices(true).should include(device)
-  end
+  context 'notifications' do
 
-  it 'should destroy all devices once the user is destroyed' do
-    user = users :one
-    device = devices :one
-    user.devices << device
-    user.save!.should be_true
-    device.save!.should be_true
-    user.destroy
-    expect { Device.find device.id }.to raise_error(ActiveRecord::RecordNotFound)
-  end
+    it 'should add a notification to the user record' do
+      user = users :one
+      notification = notifications :one
+      user.notifications << notification
+      user.save!.should be_true
+      notification.save!.should be_true
+      notification.user(true).should be_a(User)
+      user.notifications(true).should include(notification)
+    end
 
-  it 'should destroy all devices once the business user is destroyed' do
-    user = business_users :one
-    device = devices :one
-    user.devices << device
-    user.save!.should be_true
-    device.save!.should be_true
-    user.destroy
-    expect { Device.find device.id }.to raise_error(ActiveRecord::RecordNotFound)
-  end
+    it 'should add a notification to the business user record' do
+      user = business_users :one
+      notification = notifications :one
+      user.notifications << notification
+      user.save!.should be_true
+      notification.save!.should be_true
+      notification.user(true).should be_a(BusinessUser)
+      user.notifications(true).should include(notification)
+    end
 
-  it 'should add a notification to the user record' do
-    user = users :one
-    notification = notifications :one
-    user.notifications << notification
-    user.save!.should be_true
-    notification.save!.should be_true
-    notification.user(true).should be_a(User)
-    user.notifications(true).should include(notification)
-  end
+    it 'should destroy all notifications once the user is destroyed' do
+      user = users :one
+      notification = notifications :one
+      user.notifications << notification
+      user.save!.should be_true
+      notification.save!.should be_true
+      user.destroy
+      expect { Notification.find notification.id }.to raise_error(ActiveRecord::RecordNotFound)
+    end
 
-  it 'should add a notification to the business user record' do
-    user = business_users :one
-    notification = notifications :one
-    user.notifications << notification
-    user.save!.should be_true
-    notification.save!.should be_true
-    notification.user(true).should be_a(BusinessUser)
-    user.notifications(true).should include(notification)
-  end
+    it 'should destroy all notifications once the business user is destroyed' do
+      user = business_users :one
+      notification = notifications :one
+      user.notifications << notification
+      user.save!.should be_true
+      notification.save!.should be_true
+      user.destroy
+      expect { Notification.find notification.id }.to raise_error(ActiveRecord::RecordNotFound)
+    end
 
-  it 'should destroy all notifications once the user is destroyed' do
-    user = users :one
-    notification = notifications :one
-    user.notifications << notification
-    user.save!.should be_true
-    notification.save!.should be_true
-    user.destroy
-    expect { Notification.find notification.id }.to raise_error(ActiveRecord::RecordNotFound)
-  end
-
-  it 'should destroy all notifications once the business user is destroyed' do
-    user = business_users :one
-    notification = notifications :one
-    user.notifications << notification
-    user.save!.should be_true
-    notification.save!.should be_true
-    user.destroy
-    expect { Notification.find notification.id }.to raise_error(ActiveRecord::RecordNotFound)
   end
 
 end
