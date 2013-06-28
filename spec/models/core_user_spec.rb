5eab48b6ba64d75fa4e1b348632b54ba783f5336
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-06-28 14:08:50 +0100

    separated tests in core user spec

--- spec/models/core_user_spec.rb
+++ spec/models/core_user_spec.rb
@@ -8,45 +8,49 @@ describe CoreUser do
     expect { user.save! }.to raise_error(ActiveRecord::RecordInvalid)
   end
 
-  it 'should add an identity to the user record' do
-    user = users :one
-    identity = identities :one
-    user.identities << identity
-    user.save!
-    identity.save!
-    identity.user.should be_a(User)
-    user.identities(true).should include(identity)
-  end
+  context 'identities' do
 
-  it 'should add an identity to the business user record' do
-    user = business_users :one
-    identity = identities :one
-    user.identities << identity
-    user.save!
-    identity.save!
-    identity.user(true).should be_a(BusinessUser)
-    identity.user(true).name.should == user.name
-    user.identities(true).should include(identity)
-  end
+    it 'should add an identity to the user record' do
+      user = users :one
+      identity = identities :one
+      user.identities << identity
+      user.save!
+      identity.save!
+      identity.user.should be_a(User)
+      user.identities(true).should include(identity)
+    end
 
-  it 'should destroy all identities once the user is destroyed' do
-    user = users :one
-    identity = identities :one
-    user.identities << identity
-    identity.save!
-    user.save!
-    user.destroy
-    expect { Identity.find identity.id }.to raise_error(ActiveRecord::RecordNotFound)
-  end
+    it 'should add an identity to the business user record' do
+      user = business_users :one
+      identity = identities :one
+      user.identities << identity
+      user.save!
+      identity.save!
+      identity.user(true).should be_a(BusinessUser)
+      identity.user(true).name.should == user.name
+      user.identities(true).should include(identity)
+    end
 
-  it 'should destroy all identities once the business user is destroyed' do
-    user = business_users :one
-    identity = identities :one
-    user.identities << identity
-    identity.save!
-    user.save!
-    user.destroy
-    expect { Identity.find identity.id }.to raise_error(ActiveRecord::RecordNotFound)
+    it 'should destroy all identities once the user is destroyed' do
+      user = users :one
+      identity = identities :one
+      user.identities << identity
+      identity.save!
+      user.save!
+      user.destroy
+      expect { Identity.find identity.id }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'should destroy all identities once the business user is destroyed' do
+      user = business_users :one
+      identity = identities :one
+      user.identities << identity
+      identity.save!
+      user.save!
+      user.destroy
+      expect { Identity.find identity.id }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
   end
 
 end
