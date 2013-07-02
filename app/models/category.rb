9c93e31e0096cbfec298cbebd4022d984a4cf528
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-02 10:26:37 +0200

    just a little cosmetic issue :D

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
-  validates :name, :presence => true
-  validates_uniqueness_of :name
+  validates :name, :presence => true, :uniqueness => true
 end
