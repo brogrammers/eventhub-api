8a4e800b63b1ac877e197a7a15036979db776320
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-02 10:33:34 +0200

    removed visibility_type from event, so event will inherit place visibility_type

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :description, :end_time, :start_time, :name, :description, :visibility_type
+  attr_accessible :description, :end_time, :start_time, :name, :description
 
   has_many :destinations, :as => :choice, :dependent => :destroy
 
@@ -11,6 +11,10 @@ class Event < ActiveRecord::Base
   validates :description, :name, :start_time, :end_time, :place, :presence => true
   validates :name, :length => { :minimum => 5, :maximum => 256 }
   validates :description, :length => { :minimum => 5, :maximum => 1024 }
-  validates :visibility_type, :inclusion => { :in => %w(public private certified)}
   validates_with EventhubApi::Validator::Event
+
+  def visibility_type
+    self.parent.visibility_type
+  end
+
 end
