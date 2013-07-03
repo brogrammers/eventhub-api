226d9234953057af433f097d41f2d64e4c9e4506
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-03 11:39:00 +0200

    added provider enum to the identity model

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 describe Identity do
   fixtures :identities, :core_users, :users, :business_users
 
-
+  it 'should only assign provider facebook and eventhub' do
+    identity = Identity.new
+    identity.provider = :eventhub
+    identity.save!
+    identity.provider = :facebook
+    identity.save!
+    expect { identity.provider = :invalid_provider }.to raise_error(ArgumentError)
+  end
 
 end
