8b3754eaa862311a508df09bc1281d56470c76c7
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-04 16:00:20 +0100

    Added Localisation to destination model errors

--- lib/eventhub_api/validator/destination.rb
+++ lib/eventhub_api/validator/destination.rb
@@ -3,17 +3,17 @@ module EventhubApi
     class Destination < ActiveModel::Validator
       def validate(record)
         unless record.creator == record.group.creator or record.group.members.include? record.creator
-          record.errors[:creator] << 'of destination must be either a creator or member of group of this destination'
+          record.errors[:creator] << I18n.translate!('activerecord.errors.models.destination.attributes.creator.member_or_creator')
         end
 
         record.voters.each do |voter|
           unless record.group.creator == voter or record.group.members.include? voter
-            record.errors[:voters] << 'must be a creator or member of group of this destination'
+            record.errors[:voters] << I18n.translate!('activerecord.errors.models.destination.attributes.voters.member_or_creator')
           end
         end
 
         if record.voters.include? record.creator
-          record.errors[:creator] << 'of destination cannot vote for it'
+          record.errors[:creator] << I18n.translate!('activerecord.errors.models.destination.attributes.creator.cant_vote')
         end
       end
     end
