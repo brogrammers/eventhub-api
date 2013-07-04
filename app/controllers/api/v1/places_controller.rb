fecf70ad99121bb876090079a101219b14557a56
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-04 21:44:13 +0100

    added protection for me resource and removed pending tests

---
+++ app/controllers/api/v1/places_controller.rb
@@ -0,0 +1,9 @@
+module Api
+  module V1
+    class PlacesController < BaseController
+
+
+
+    end
+  end
+end
--- spec/requests/authorization_spec.rb
+++ spec/requests/authorization_spec.rb
@@ -3,14 +3,11 @@ require 'spec_helper'
 describe 'Authorization' do
 
   it 'should respond with 401 if no access token is attached' do
-    pending 'Once we have a Controller we can include this test'
     get('/api/v1/me').should == 401
   end
 
   it 'should respond with 401 if invalid access token is attached' do
-    pending 'Once we have a Controller we can include this test'
-    @request.env['HTTP_AUTHORIZATION'] = 'Bearer invalid token'
-    get('/api/v1/me').should == 401
+    get('/api/v1/me', nil, { 'HTTP_AUTHORIZATION' => 'Bearer invalid token' }).should == 401
   end
 
 end
