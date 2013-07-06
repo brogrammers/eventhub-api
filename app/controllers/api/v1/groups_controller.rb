de45d4e2146fa0d3bbcbab1a8fe39256302503d1
robertlis <robert.lis2@mail.dcu.ie>
2013-07-06 17:53:51 +0100

    Added members controller

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -2,35 +2,51 @@ module Api
   module V1
     class GroupsController < BaseController
 
+      before_filter :can_user_access_group?, :only => [:update, :show, :destroy]
+      before_filter :can_user_modify_group?, :only => [:update, :destroy]
+
       def index
         @groups = Group.all
       end
 
       def create
-        @group = Group.new :name => params[:name], :description => params[:description]
-        @current_user.groups_created << group
+        @group = Group.new(group_params)
+        @current_user.groups_created << @group
         @group.save!
       end
 
       def update
         @group = Group.find params[:id]
-        raise ActiveRecord::RecordNotFound unless @group.can_be_modified_by? @current_user
-        @group.name = params[:name] unless params[:name].nil?
-        @group.description = params[:description] unless params[:description].nil?
+        @group.update_attributes!(group_params)
         @group.save!
       end
 
       def show
-        @group = Group.find params[:id]
-        raise ActiveRecord::RecordNotFound unless @group.can_be_seen_by? @current_user
+        @group = Group.find(params[:id])
         @group
       end
 
       def destroy
         @group = Group.find params[:id]
-        raise ActiveRecord::RecordNotFound unless @group.can_be_modified_by? @current_user
         @group.destroy
       end
+
+      private
+
+      def can_user_access_group?
+        @group = Group.find params[:id]
+        raise ActiveRecord::RecordNotFound unless @group.can_be_seen_by? @current_user
+      end
+
+      def can_user_modify_group?
+        @group = Group.find params[:id]
+        raise  unless @group.can_be_modified_by? @current_user
+      end
+
+      def group_params
+        params.permit({:name => []}, :description)
+      end
+
     end
   end
 end
