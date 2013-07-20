8f875e10c1db34991f5c952421f8e84837072ca3
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-20 17:51:34 +0100

    added places controller + tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ EventhubApi::Application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :me, :only => [:index]
+      resources :places, :only => [:index, :show, :create, :update, :destroy]
     end
   end
 end
