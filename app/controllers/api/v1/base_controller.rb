353dcd97168277a7b0a512f92b4846cb12e1f31f
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-22 21:36:55 +0100

    added filter to save user location if given

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,13 @@
 module Api
   module V1
     class BaseController < ApplicationController
+      before_filter :save_user_location, :if => lambda { request.env['user.location'] }
 
-
+      def save_user_location
+        @current_user.location.latitude = request.env['user.location'][:latitude]
+        @current_user.location.longitude = request.env['user.location'][:longitude]
+        @current_user.save! #TODO: We should think of a global place to save the current user. We dont want to invoke .save! too much!
+      end
 
     end
   end
