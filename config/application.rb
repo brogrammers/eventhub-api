9d093ef96e2f4a7c9408e6aa9a61adbd5bff7d4a
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-22 21:35:47 +0100

    added middleware to the rack stack

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,8 @@ module EventhubApi
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.middleware.use EventhubApi::Middleware::LocationDetector
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
